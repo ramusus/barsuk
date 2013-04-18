89331d008e0b85d93094c4e7cf20d87557e31f9b
ramusus <ramusus@gmail.com>
2013-04-18 21:20:50 +0400

    added responses

--- project/media/js/common.js
+++ project/media/js/common.js
@@ -1,5 +1,15 @@
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function show_review() {
+    if(reviews) {
+        var review = reviews[getRandomInt(0, reviews.length-1)]
+        $('.reviews-items').html('<p>' + review + '</p>');
+        setTimeout(show_review, 10000);
+    }
+}
+
 $(function() {
-    $('.fancybox').fancybox({
-//        'modal' : true
-    });
+    $('.fancybox').fancybox({});
+    show_review();
 });
